8e461243252381a7a580f77f8067104ae04b11c0
facus027 <facussguelis@gmail.com>
2024-05-09 21:07:08 -0300

    starting models

---
+++ app/order/layout.tsx
@@ -0,0 +1,23 @@
+import OrderSidebar from "@/components/order/OrderSidebar";
+import OrderSumary from "@/components/order/OrderSumary";
+import React from "react";
+
+export default function layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <div className=" md:flex">
+        <OrderSidebar />
+
+        <main className=" md:flex-1 md:h-screen md:overflow-y-scroll p-5">
+          {children}
+        </main>
+
+        <OrderSumary />
+      </div>
+    </>
+  );
+}
---
+++ app/products/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ProductNewpage() {
+  return <div>ProductNewpage</div>;
+}
